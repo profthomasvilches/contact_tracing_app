30a0a93952602fc8e3906b3d143529675120ff0e
Taiye <tkommixx@gmail.com>
2025-06-23 14:16:14 -0400

    Added app coverage parameter to function and implemented for loop.

--- scen.jl
+++ scen.jl
@@ -1,4 +1,4 @@
-# Taiye (2025.06.17)
+# Taiye (2025.06.23)
 
 # Taiye (2025.05.30): run_param_scen_cal has 16 inputs here but 21 in simulations_cluster.jl.
 
@@ -8,5 +8,10 @@
 
 #0.35 for omicron generates a R0 of 0.84
 #b::Float64,province::String="ontario",ic1::Int64=1,index::Int64 = 0,scen::Int64 = 0,test_time::Int64=0,test_dur::Int64=0,mt::Int64=300,nsims::Int64=500)
+
+a_c = 0.05 # Taiye (2025.06.23): app_coverage
 cv.ModelParameters(popsize=10)
-run_param_scen_cal(0.1,"ontario",1,0,10,50,200,2,1000)
+for i = 0:20
+    run_param_scen_cal(0.1,"ontario",1,0,10,50,200,21000,a_c*i)
+end
+# run_param_scen_cal(0.1,"ontario",1,0,10,50,200,2,1000)
