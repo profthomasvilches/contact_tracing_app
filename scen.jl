52da82640c18bbcf2f88d5d06fe572693cc32f32
Taiye <tkommixx@gmail.com>
2025-08-05 19:57:03 -0400

    Additional parameters added to function and all scenarios now run together.

--- scen.jl
+++ scen.jl
@@ -12,15 +12,40 @@
 a_c = 0.05 # Taiye (2025.06.23): app_coverage
 cv.ModelParameters(popsize=10)
 
+# Taiye (2025.07.22) bta = 0.1 [Attempting to reduce R0]
+# bta = 0.095
+bta = 0.1
+
 # Taiye (2025.07.01): Uncomment after rectifying coverage.
+for i = 0:5
+    #for j = 1:10
+
+    # 100% Sensitivity
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,0,false,1) # 1 test no notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,0,true,1) # 1 test notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,1,false,1) # 1 test no notification - random contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,1,true,1) # 1 test notification - random contacts for isolation
+    
+    # Set sensitivity back to random number
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,0,false,0) # 1 test no notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,0,true,0) # 1 test notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,1,false,0) # 1 test no notification - random contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,1,1,true,0) # 1 test notification - random contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,2,0,false,0) # 2 test no notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,2,0,true,0) # 2 test notification - zero contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,2,1,false,0) # 2 test no notification - random contacts for isolation
+     run_param_scen_cal(bta,"ontario",1,0,1,200,300,500,10000,0.2*i,2,1,true,0) # 2 test notification - random contacts for isolation
+     # end
+end
+
+#run_param_scen_cal(0.1,"ontario",1,0,1,200,300,1,1000,0.6)
+#run_param_scen_cal(0.1,"ontario",1,1,1,200,500,500,10000,0.5)
+#run_param_scen_cal(0.1,"ontario",1,2,1,200,500,500,10000,0.8)
+#run_param_scen_cal(0.1,"ontario",1,3,1,200,500,500,10000,1.0)
+
+# Taiye (2025.07.09): Alternating beta (uncomment)
 #for i = 0:20
-    # run_param_scen_cal(0.1,"ontario",1,0,10,50,200,2,1000,a_c*i)
- #   run_param_scen_cal(1.0,"ontario",1,0,1,200,200,500,1000,a_c*i)
+ #   for j = 1:10
+  #   run_param_scen_cal(bta*j,"ontario",1,0,1,200,300,500,10000,a_c*i)
+   # end
 #end
-
-run_param_scen_cal(0.1,"ontario",1,0,1,200,300,500,10000,0.0)
-run_param_scen_cal(0.1,"ontario",1,1,1,200,300,500,10000,0.5)
-run_param_scen_cal(0.1,"ontario",1,2,1,200,300,500,10000,0.8)
-run_param_scen_cal(0.1,"ontario",1,3,1,200,300,500,10000,1.0)
-
-# run_param_scen_cal(0.1,"ontario",1,0,10,50,200,2,1000)
